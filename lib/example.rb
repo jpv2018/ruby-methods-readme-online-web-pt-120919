3a37cd18268693d1debd0d7ab789a864e713dea2
Jason Viola <jason.viola@mavs.uta.edu>
2019-12-09 14:03:18 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,14 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  count = 0
+  while count <= 10 do
+    puts phrase
+    count += 1
+  end
+end
+  
+  
